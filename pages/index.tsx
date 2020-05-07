fe65820f02944015f78b59f163a1ba1bd919f547
Yuichi Tateno <hotchpotch@gmail.com>
2020-05-07 09:07:01 +0900

    Update example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
+import { useState, useEffect } from "react";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
-
 import { collection, get, Doc } from "typesaurus";
-import { useState, useEffect } from "react";
 
-const firebaseConfig = require("../firebase.config");
+const firebaseConfig = require("../firebase.config").default;
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
